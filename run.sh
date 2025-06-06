9ebb993cab2240bf07d9fb3f0766299109cf0a8f
joseravera <jravera@itba.edu.ar>
2025-06-06 18:41:42 -0300

    comentario en run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,10 @@
 # qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker
 
 
+# este sudo lo tengo que hacer cada vez desp de compilar, sino no puedo correrlo sin hacer: [sudo ./run.sh]
+# si no hago el sudo, y directamente lo corro asi: [sudo ./run.sh] corre todo bien pero no anda el sonido 
+# sudo chown $USER:$USER Image/x64BareBonesImage.qcow2
+
 if [ "$(uname)" == "Darwin" ]; then
     audio_driver="coreaudio"
 else
