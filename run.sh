7af1834811e55c453ab40741d8efed5d1e8d9f13
joseravera <jravera@itba.edu.ar>
2025-06-06 18:36:34 -0300

    ahi deberia funcionar en mac tb

--- run.sh
+++ run.sh
@@ -2,4 +2,17 @@
 
 # qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512
 
-qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker
+# qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker
+
+
+if [ "$(uname)" == "Darwin" ]; then
+    audio_driver="coreaudio"
+else
+    audio_driver="pa"
+fi
+
+qemu-system-x86_64 \
+  -hda Image/x64BareBonesImage.qcow2 \
+  -m 512 \
+  -audiodev ${audio_driver},id=speaker \
+  -machine pcspk-audiodev=speaker
